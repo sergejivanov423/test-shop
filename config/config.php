241575d4a710c9dff3d0b6699a8f482720c4d08b
sergejivanov423 <sergejivanov423@gmail.com>
2016-07-17 17:38:07 +0300

    add config.php file with base DB config connect. Also includes constants for Controllers and Views

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+// Database configs
+define('HOST','test-shop.loc/');
+define('DB_NAME','test_shop');
+define('USER','vodolserge');
+define('PASSWORD','admin');
+
+// Path to Controllers folder
+define('CONTROLLER',__DIR__.'/app/core/controllers/');
+
+// Path to Views folder
+define('VIEWS',__DIR__.'/app/core/views/');
+
