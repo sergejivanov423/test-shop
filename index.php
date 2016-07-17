3c66b9f8989549254a7720f68315ec389ae01b8c
sergejivanov423 <sergejivanov423@gmail.com>
2016-07-17 19:16:32 +0300

    require autoloader and routes [index.php]

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 // Point of entry
 
+use ShopApp\Components\Router;
+
 // Common config
 ini_set('display_errors',1);
 error_reporting(E_ALL);
+
+// Autoload classes and files
+require_once(__DIR__.'/vendor/autoload.php');
+$routes = require_once(__DIR__.'/config/routes.php');
+
+
