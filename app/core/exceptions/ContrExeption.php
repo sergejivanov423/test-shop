cc878613b63e7a9c96291747419d2878c7937a8d
sergejivanov423 <sergejivanov423@gmail.com>
2016-07-18 21:24:45 +0300

    [FIX] Docs for class ContrExeption

--- app/core/exceptions/ContrExeption.php
+++ app/core/exceptions/ContrExeption.php
@@ -3,6 +3,10 @@
 namespace ShopApp\Exceptions;
 
 
+/**
+ * Custom Exeption for Controllers
+ * @package ShopApp\Exceptions
+ */
 class ContrExeption extends \Exception
 {
     public function __construct($text) {
