11452f14f78ee45af4a39e78d4d1d24e02460b44
sergejivanov423 <sergejivanov423@gmail.com>
2016-07-18 22:27:31 +0300

    add Router class that implements a method Run() in which assigns Controllers, Actions and Params

--- app/core/components/Router.php
+++ app/core/components/Router.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace ShopApp\Components;
+use ShopApp\Exceptions\ContrExeption;
 
 /**
  * Class Router
@@ -19,4 +20,75 @@ class Router
         $this->routes = $routes;
     }
 
+    /**
+     * Parse the address bar(URL):
+     * 1. Iterate array of routes and look for key matches in a URL
+     * 2. If OK ? Replace of a match and key value
+     * 3. Then get an internal route.
+     * 4. Convert an internal route string to an array
+     * 5. The array elements are our CONTROLLER, ACTION, and PARAMETERS
+     * 6. Use Reflection Api and call Controller Action
+     *
+     */
+    public function run()
+    {
+        // The relative path excluding the host name
+        $path = substr($_SERVER['PHP_SELF'],0,strpos($_SERVER['PHP_SELF'],'index.php'));
+
+        // RequestURL excluding the script file[index.php]
+        $request = $this->getURL();
+
+        if(SITE_URL === $path) {
+
+            $realRoute = (string) substr($request,strlen(SITE_URL));
+
+            foreach ($this->routes as $urlPattern => $path) {
+
+                if (preg_match("~$urlPattern~", $realRoute)) {
+
+                    $internalRoute = preg_replace("~$urlPattern~", $path, $realRoute);
+                    $internalRoute = trim($internalRoute,'/');
+                    $parts = explode('/', $internalRoute);
+
+                    $controllerName = 'ShopApp\\Controllers\\' . ucfirst(array_shift($parts)) . 'Controller';
+                    $actionName = 'action' . ucfirst(array_shift($parts));
+                    $urlParams = $parts;
+
+                    if (class_exists($controllerName)) {
+
+                        $ref = new \ReflectionClass($controllerName);
+
+                        if ($ref->hasMethod($actionName)) {
+
+                            if ($ref->isInstantiable()) {
+                                $controller = $ref->newInstance();
+                                $action = $ref->getMethod($actionName);
+                                $callAction = $action->invokeArgs($controller, $urlParams);
+                                break;
+
+                            }
+                        }
+                    }
+                } else {
+
+                    throw new ContrExeption(
+                        '<p style="color:red">
+                            Неверно заданы маршруты
+                            в конфигурационном файле
+                        </p>'
+                    );
+                }
+            }
+        }
+    }
+
+    /**
+     * @return string Trimed RequestURI
+     */
+    private function getURL()
+    {
+        if (!empty($_SERVER['REQUEST_URI'])) {
+            return trim($_SERVER['REQUEST_URI'],"/");
+        }
+    }
 }
