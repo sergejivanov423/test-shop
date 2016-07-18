ec38998df462c9bfeaf43240fa1ebadf0a88b92e
sergejivanov423 <sergejivanov423@gmail.com>
2016-07-18 14:39:16 +0300

    call method Router->run() and wrap it up in [try..catch] block in which the ContrException is thrown

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 // Point of entry
 
 use ShopApp\Components\Router;
+use ShopApp\Exceptions\ContrExeption;
 
 // Common config
 ini_set('display_errors',1);
@@ -11,4 +12,14 @@ error_reporting(E_ALL);
 require_once(__DIR__.'/vendor/autoload.php');
 $routes = require_once(__DIR__.'/config/routes.php');
 
+
+try{
+    // Call Router
+    $router = new Router($routes);
+    $router->run();
+
+} catch (ContrExeption $e) {
+    echo $e->getMessage();
+    return;
+}
 
