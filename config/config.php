57b656ec59f36081b15da68b9ff0aba7a1bda6c7
sergejivanov423 <sergejivanov423@gmail.com>
2016-08-16 19:50:13 +0300

    define TPL constant

--- config/config.php
+++ config/config.php
@@ -1,16 +1,19 @@
 <?php
 
 // Database configs
-define('HOST','test-shop.loc/');
-define('DB_NAME','test_shop');
-define('USER','vodolserge');
-define('PASSWORD','admin');
+define('HOST', 'localhost');
+define('DB_NAME', 'phpshop');
+define('USER', 'vodolserge');
+define('PASSWORD', 'admin');
 
 // Path to Controllers folder
-define('CONTROLLER',dirname(__DIR__).'/app/core/controllers/');
+define('CONTROLLER', dirname(__DIR__).'/app/core/controllers/');
 
 // Path to Views folder
-define('VIEWS',dirname(__DIR__).'/app/core/views/');
+define('VIEWS', dirname(__DIR__).'/app/core/views/');
 
 // Relative path (without name of host)
 define('SITE_URL','/');
+
+// Path to [template] folder
+define('TPL', SITE_URL.'app/template/web/');
